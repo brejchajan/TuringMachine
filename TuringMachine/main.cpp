f05b01f2f79f3c010f109aa871f209075b7dcb05
Jan Brejcha <brejchaja@gmail.com>
2013-06-10 19:51:43 +0200

    Initial Commit

---
+++ TuringMachine/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TuringMachine
+//
+//  Created by Jan Brejcha on 10.06.13.
+//  Copyright (c) 2013 brejchajan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
